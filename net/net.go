a161ed05424327c0a35f7a48860bbf6f95e1ffed
autom8ter <admin@autom8ter.com>
2019-03-11 08:27:21 -0600

    net

--- net/net.go
+++ net/net.go
@@ -1,1 +1,91 @@
 package net
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func ClientAPI(req *http.Request) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		client := &http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err.Error())
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Fprintf(w, string(body))
+	}
+}
+
+func ClientAPIWithJWT(req *http.Request, signKey, clientName string, exp *time.Time) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		client := &http.Client{}
+		validToken, err := GenerateJWT(signKey, clientName, exp)
+		if err != nil {
+			fmt.Println("Failed to generate token")
+		}
+		req.Header.Set("Token", validToken)
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err.Error())
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Fprintf(w, string(body))
+	}
+}
+
+func GenerateJWT(signKey, client string, exp *time.Time) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["client"] = client
+	claims["exp"] = exp
+
+	tokenString, err := token.SignedString(signKey)
+
+	if err != nil {
+		fmt.Errorf("Something Went Wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func JWTAuthorized(signKey string, endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return signKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
+}
