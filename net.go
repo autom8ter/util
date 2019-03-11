72615a0c0a081e3acd165568a0cc7bd3250b8263
autom8ter <admin@autom8ter.com>
2019-03-11 10:59:26 -0600

    vars

--- net.go
+++ net.go
@@ -65,3 +65,15 @@ func WithSettings(r *mux.Router)  {
 		w.Write([]byte(ToPrettyJsonString(viper.AllSettings())))
 	})
 }
+
+func WithStaticViews(r *mux.Router){
+	// On the default page we will simply serve our static index page.
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+}
+
+func RequestVars(req *http.Request) map[string]string {
+	return mux.Vars(req)
+}
