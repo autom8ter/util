ed969155129262d88ccf5d3a43184ade1bdc66b1
autom8ter <admin@autom8ter.com>
2019-03-13 00:17:18 -0600

    grpc

--- netutil/grpc/api/server.go
+++ netutil/grpc/api/server.go
@@ -8,22 +8,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-type RegisterServerFunc func(*grpc.Server)
-type RegisterHandlerFunc func(context.Context, *runtime.ServeMux, *grpc.ClientConn) error
-
-type Driver struct {
-	RegisterServerFunc  func(*grpc.Server)
-	RegisterHandlerFunc func(context.Context, *runtime.ServeMux, *grpc.ClientConn) error
-}
-
-func (d *Driver) RegisterWithServer(s *grpc.Server) {
-	d.RegisterServerFunc(s)
-}
-
-func (d *Driver) RegisterWithHandler(ctx context.Context, m *runtime.ServeMux, cc *grpc.ClientConn) error {
-	return d.RegisterHandlerFunc(ctx, m, cc)
-}
-
 // Server is an interface for representing gRPC server implementations.
 type Server interface {
 	RegisterWithServer(*grpc.Server)
