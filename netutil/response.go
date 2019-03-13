af0878be97504e7f94ea5d3e818fd1fea90b5759
autom8ter <admin@autom8ter.com>
2019-03-13 15:43:36 -0600

    response

--- netutil/response.go
+++ netutil/response.go
@@ -1,6 +1,9 @@
 package netutil
 
-import "net/http"
+import (
+	"io/ioutil"
+	"net/http"
+)
 
 func SetResponseHeaders(headers map[string]string, w http.ResponseWriter) {
 	for k, v := range headers {
@@ -9,4 +12,9 @@ func SetResponseHeaders(headers map[string]string, w http.ResponseWriter) {
 }
 func GetHeader(key string, w http.ResponseWriter) string {
 	return w.Header().Get(key)
+}
+
+func ReadBody(resp *http.Response) ([]byte, error){
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
